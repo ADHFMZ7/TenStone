245a7a89ce58fa566c07ea3df69a3bbcc499359c
adhfmz7 <ahmadaldasouqi559@gmail.com>
2024-05-31 22:29:40 -0700

    Basic backward pass added

--- TenStone/tensor.py
+++ TenStone/tensor.py
@@ -1,25 +1,88 @@
 """
 Implements the Tensor class.
 
+TODO:
+    Make sure numpy error handling works
+    Write unit tests
+    Add documentation 
+
 """
+import numpy as np
+
 
 
 class Tensor:
 
-    def __init__(self, shape):
-        # if !valid_shape(shape):
-            # raise Exception("Invalid shape") 
+    def __init__(self, data, _children=()):
+        if isinstance(data, list):
+            data = np.array(data, dtype=np.single)
+        elif not isinstance(data, np.ndarray):
+            raise ValueError("Input data must be a NumPy array")
 
-        def dfs(ix):
-            if ix >= len(shape) - 1:
-                return [0 for _ in range(shape[ix])]
+        # Information for autograd
+        self.grad = 0
+        self._backward = lambda: None
+        self._prev = set(_children)
 
-            return [dfs(ix + 1) for _ in range(shape[ix])]
+        self.data = data
 
-        print(dfs(0))
+    @property
+    def shape(self):
+        return self.data.shape
 
-        self.data = []
-        
+    @property
+    def size(self):
+        return self.data.size
+
+    def get(self, indices):
+        print(self.data, indices)
+        return self.data[tuple(indices)]
+
+    def set(self, indices, value):
+        self.data[tuple(indices)] = value
+
+    def __repr__(self):
+        return self.data.__str__()
+
+    def __getitem__(self, indices):
+        return self.get(indices)
+
+    def __setitem__(self, indices, new_value):
+        self.set(indices, new_value)
+
+    
+
+# Unary Ops
+
+    def sum(self):
+
+        ret = Tensor(self.data.sum(keepdims=True).squeeze(), _children=(self, ))
+
+        def _backward():
+            self.grad = np.ones_like(self.data)
+
+        ret._backward = _backward
+        return ret
+
+# Binary Ops
+
+    def dot(self, other):
+      
+        ret = Tensor(np.dot(self.data, other.data), _children=(self, other))
+
+        def _backward():
+
+            print(self.shape, other.shape, ret.shape)
+
+            self.grad = ret.grad @ other.data.T
+            other.grad = self.data.T @ ret.grad
+
+        ret._backward = _backward
+        return ret
+
+
+
+# CONSTRUCTORS
 
     @classmethod
     def zeros(cls, shape):
@@ -27,7 +90,6 @@ class Tensor:
         # ret = cls(shape)
         # ret.data = np.zeros(shape)
         # return ret
-        
 
     @classmethod
     def ones(cls, shape):
@@ -35,13 +97,21 @@ class Tensor:
         # ret = cls(shape)
         # ret.data = np.ones(shape)
         # return ret
-      
-    @classmethod
-    def from_data(cls):
-        ...
 
+
+    def backward(self):
+        ...
+        # Topo sort and then call _backward in reverse order
 
 if __name__ == "__main__":
-    Tensor([2, 2])
-    # a = Tensor.ones((10, 10))
-    # b = Tensor.zeros(a.data.shape)
+    A = Tensor([[1, 2, 3], [4, 5, 6]])
+    B = Tensor([[1, 2], [3, 4], [5, 6]])
+
+    C = A.dot(B)
+    D = C.sum()
+
+    D._backward()
+    C._backward()
+
+    print(A.grad)
+    print(B.grad)
